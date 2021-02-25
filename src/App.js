8041b4602564438e86ba592ea0e31a1f4a53541a
Van Slaars, Andy L <andy.vanslaars@cengage.com>
2021-02-25 15:34:03 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function App() {
+  return <div>This is the most basic React app</div>
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
